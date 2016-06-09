2af1779d8c124b67436b70af02ee4d716ce8e789
John Griffiths <john@uxgent.co>
2016-06-09 19:51:45 -0400

    contact form.

--- htdocs/perch/templates/layouts/home.contact.php
+++ htdocs/perch/templates/layouts/home.contact.php
@@ -2,36 +2,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12 quote-form">
-        <form id="contact-form" class="col-xs-12 col-md-6">
-					<div class="row control-group">
-						<div class="form-group floating-label-form-group controls">
-							<label>Name</label>
-							<input type="text" name="name" class="form-control" placeholder="Name" id="name" required data-validation-required-message="Please enter your name.">
-							<p class="help-block text-danger"></p>
-						</div>
-					</div>
-					<div class="row control-group">
-						<div class="form-group floating-label-form-group controls">
-							<label>Email Address</label>
-							<input type="email" name="email" class="form-control" placeholder="Email Address" id="email" required data-validation-required-message="Please enter your email address.">
-							<p class="help-block text-danger"></p>
-						</div>
-					</div>
-					<div class="row control-group">
-						<div class="form-group floating-label-form-group controls">
-							<label>Message</label>
-							<textarea rows="5" name="message" class="form-control" placeholder="Message" id="message" required data-validation-required-message="Please enter a message."></textarea>
-							<p class="help-block text-danger"></p>
-						</div>
-					</div>
-					<br>
-					<div id="success"></div>
-					<div class="row">
-						<div class="form-group">
-							<button type="submit" class="btn btn-success btn-lg">Send</button>
-						</div>
-					</div>
-				</form>
+        <?php perch_content("Contact Form") ?>
         <div class="contact-form-details col-xs-12 col-md-6">
           <div id="contact-message">
       			<div id="contact-message-content">
