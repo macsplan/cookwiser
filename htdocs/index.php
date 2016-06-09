1555ec14864fa2e41a324131c4d3f42bc2ac77bc
John Griffiths <john@uxgent.co>
2016-06-09 19:36:17 -0400

    analytics.

--- htdocs/index.php
+++ htdocs/index.php
@@ -31,5 +31,16 @@
     <?php perch_layout('home.about'); ?>
     <?php perch_layout('home.contact'); ?>
     <?php perch_layout('home.footer'); ?>
+
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-76663248-3', 'auto');
+      ga('send', 'pageview');
+
+    </script>
   </body>
 </html>
